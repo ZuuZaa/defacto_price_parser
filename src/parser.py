b2ede60099fccc45c95c83a221e27ef1174d466d
Zumrud <zumrud@IB-MacBook-Pro.local>
2021-02-11 22:23:01 +0400

    new commit

--- src/parser.py
+++ src/parser.py
@@ -6,13 +6,13 @@ from helper_def import find_all_links, product_parser
 
 link = 'https://www.defacto.com.tr'
 items = find_all_links(link, headers,'nav','mainmenu')
-print(items)
-#for a in items:
- #   href = a.get('href')
- #   category_responce = requests.get(f'{link}/{href}').text
-  #  new_soup = BeautifulSoup(category_responce, 'lxml')
-  #  product_list = new_soup.find_all('div', class_='products-card')
-  #  product_parser(product_list, 'data-documents', 'ProductName', 'ProductPriceInclTax')
+
+for a in items:
+    href = a.get('href')
+    category_responce = requests.get(f'{link}/{href}').text
+    new_soup = BeautifulSoup(category_responce, 'lxml')
+    product_list = new_soup.find_all('div', class_='products-card')
+    product_parser(product_list, 'data-documents', 'ProductName', 'ProductPriceInclTax')
 
 
 
